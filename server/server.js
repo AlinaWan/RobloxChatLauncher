f8417f5fe6d78895f2811115efdeee9c5b956803
AlinaWan <wuangelina40@gmail.com>
2026-01-04 17:58:30 -0800

    fix(server): properly trust proxy for Render

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 const rateLimit = require('express-rate-limit');
 
-// Trust first proxy (Render / Heroku / etc.)
-app.set('trust proxy', 1); // '1' = trust the first proxy in front of us
+// Trust proxy (Render / Heroku / etc.)
+app.set('trust proxy', true);
 
 // Middleware to parse plain text bodies (sent by C# client)
 app.use(express.text());
