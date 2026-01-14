2272d1fe7312a61a68a433f284d4b13c86f90f14
Alina <wuangelina40@gmail.com>
2026-01-14 02:56:14 -0800

    chore(client): Remove outdated comments in Program.cs

--- client/Program.cs
+++ client/Program.cs
@@ -28,24 +28,9 @@ class Program
         string uri = RobloxChatLauncher.Utils.LaunchData.LaunchUri;
         string gameId = RobloxChatLauncher.Utils.LaunchData.GetGameId();
 
-        // Console.WriteLine("Roblox Launch Detected");
-
-        // DEBUG: Print full URI for inspection
-        //
-        // The gameId only appears if you join a server directly from the servers tab,
-        // not if you press the play button on the game's main page.
-        //
-        // If we can't find the gameId frm the URI, we'll just put the user
-        // in a global channel and prompt them to join a server directly to join
-        // a server-scoped channel.
-        // We can also let the user switch channels with `/` commands.
-        //
-        // Console.WriteLine(uri);
-        // Console.WriteLine(gameId);
-
-        // DEBUG: Keep the console open for inspection
-        // Console.WriteLine("Keeping console open for inspection. Press Enter to continue launching Roblox...");
-        // Console.ReadLine();
+        // We use RobloxChatLauncher.Services.RobloxAreaService
+        // to detect the JobId now.
+        // Do NOT use the gameId from the URI anymore
 
         string robloxExe = RobloxChatLauncher.Utils.RobloxLocator.ResolveRobloxPlayerPath();
         if (robloxExe == null)
