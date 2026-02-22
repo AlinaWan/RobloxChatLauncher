09c6b3f31de41930ad8efabd26271fb925562e2d
AlinaWan <wuangelina40@gmail.com>
2026-02-22 01:54:34 -0800

    feat(client): Add compatibility for bootstrappers Currently looks for Bloxstrap, Voidstrap, and Fishstrap, else uses vanilla Roblox

---
+++ client/LaunchData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobloxChatLauncher.Utils
+{
+    public static class LaunchData
+    {
+        public static string LaunchUri
+        {
+            get; set;
+        }
+    }
+}
--- client/RobloxLocator.cs
+++ client/RobloxLocator.cs
@@ -1,72 +1,69 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Web;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
 
 namespace RobloxChatLauncher.Utils
 {
+    // Helps differentiate between the vanilla Roblox client and bootstrappers
+    // public enum my beloved
+    public enum RobloxClientType
+    {
+        Vanilla,
+        Bootstrapper
+    }
+
+    public record RobloxClientInfo(string ExecutablePath, RobloxClientType Type);
+
     public static class RobloxLocator
     {
-        public static string GetRobloxVersionFolder()
+        // Resolves either a bootstrapper client or the vanilla Roblox client
+        public static RobloxClientInfo ResolveRobloxPlayer()
         {
+            // List of known bootstrappers to check for in the registry. The first one found will be used.
+            string[] bootstrappers = { "Bloxstrap", "Voidstrap", "Fishstrap" };
+
+            foreach (string bootstrapper in bootstrappers)
+            {
+                // Check HKCU\Software for the bootstrapper's InstallLocation. If it exists, we assume it's a valid Roblox client and return it.
+                using var key = Registry.CurrentUser.OpenSubKey($@"Software\{bootstrapper}");
+                string installPath = key?.GetValue("InstallLocation") as string;
+
+                if (!string.IsNullOrEmpty(installPath))
+                {
+                    // Assume the executable is located at InstallLocation\{bootstrapper}.exe
+                    string exePath = Path.Combine(installPath, $"{bootstrapper}.exe");
+                    if (File.Exists(exePath))
+                        return new RobloxClientInfo(exePath, RobloxClientType.Bootstrapper);
+                }
+            }
+
+            // Else return the vanilla RobloxPlayerBeta.exe
+            string vanillaPath = ResolveVanillaRobloxPlayerPath();
+            if (vanillaPath != null)
+                return new RobloxClientInfo(vanillaPath, RobloxClientType.Vanilla);
+
+            return null;
+        }
+
+        public static string GetVanillaRobloxVersionFolder()
+        {
+            // This key stores the version folder name of the currently installed Roblox, which is used to construct the path to RobloxPlayerBeta.exe
             using var key = Registry.ClassesRoot.OpenSubKey(@"roblox-player\shell\open\command");
             return key?.GetValue("version") as string;
         }
 
-        public static string ResolveRobloxPlayerPath()
+        public static string ResolveVanillaRobloxPlayerPath()
         {
-            var versionFolder = GetRobloxVersionFolder();
+            var versionFolder = GetVanillaRobloxVersionFolder();
             if (string.IsNullOrEmpty(versionFolder))
                 return null;
 
+            // Construct the path to RobloxPlayerBeta.exe based on the version folder
             var exePath = Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
                 "Roblox", "Versions", versionFolder, "RobloxPlayerBeta.exe");
 
             return File.Exists(exePath) ? exePath : null;
-        }
-    }
-    public static class LaunchData
-    {
-        public static string LaunchUri
-        {
-            get; set;
-        }
-        public static string GetGameId()
-        {
-            if (string.IsNullOrEmpty(LaunchUri))
-                return null;
-
-            try
-            {
-                // 1. Find the 'placelauncherurl:' section
-                string key = "placelauncherurl:";
-                int startIndex = LaunchUri.IndexOf(key, StringComparison.OrdinalIgnoreCase);
-                if (startIndex == -1)
-                    return null;
-
-                string encodedUrl = LaunchUri.Substring(startIndex + key.Length);
-
-                // Stop at the next '+' which separates segments (if it exists)
-                int plusIndex = encodedUrl.IndexOf('+');
-                if (plusIndex != -1)
-                    encodedUrl = encodedUrl.Substring(0, plusIndex);
-
-                // 2. Decode the URL (converts %3F, %3D, etc.)
-                string decodedUrl = HttpUtility.UrlDecode(encodedUrl);
-
-                // 3. Extract gameId from decoded query string
-                // We decode first, then look for "gameId=" literally
-                Uri uri = new Uri(decodedUrl);
-                var queryParams = HttpUtility.ParseQueryString(uri.Query);
-
-                return string.IsNullOrEmpty(queryParams["gameId"]) ? null : queryParams["gameId"];
-            }
-            catch
-            {
-                // Any parsing errors just return null
-                return null;
-            }
         }
     }
 }
