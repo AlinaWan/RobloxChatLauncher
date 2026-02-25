c095431ed73c33fe28d59afd25d27b775a790e8f
AlinaWan <wuangelina40@gmail.com>
2026-02-25 03:43:16 -0800

    refactor(client): Move SmoothPanel to UI namespace

--- client/ChatForm/ChatForm.UI.cs
+++ client/ChatForm/ChatForm.UI.cs
@@ -521,26 +521,4 @@ namespace RobloxChatLauncher
             base.OnFormClosing(e);
         }
     }
-
-    // --------------------------------------------------
-    // Smooth Panel and TextBox (for reducing flicker)
-    // --------------------------------------------------
-    public class SmoothPanel : Panel
-    {
-        public SmoothPanel()
-        {
-            this.DoubleBuffered = true;
-            this.SetStyle(ControlStyles.AllPaintingInWmPaint | // Don't add ControlStyles.UserPaint here or it will render garbage pixels when resizing the window
-                          ControlStyles.OptimizedDoubleBuffer, true);
-        }
-    }
-    public class SmoothTextBox : TextBox
-    {
-        public SmoothTextBox()
-        {
-            this.DoubleBuffered = true;
-            // This stops the background from "flashing" dark before the text draws
-            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
-        }
-    }
 }
