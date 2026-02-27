57a577d6d8947a64e21bac525206001c53f1ce8f
Alina <wuangelina40@gmail.com>
2026-02-26 21:04:19 -0800

    chore(server): Fix comment to clarify MAIL_TTL_MS unit

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 10000,
     MAX_QUEUE_SIZE: 100, // Max number of pending messages in the queue before we start rejecting new ones
-    MAIL_TTL_MS: 5_000, // seconds before mail is auto-deleted
+    MAIL_TTL_MS: 5_000, // milliseconds before mail is auto-deleted
     TEXT_LIMIT_BYTES: 1024,
     WEBSOCKET_LIMIT_BYTES: 1024,
     HEARTBEAT_INTERVAL: 30_000,
